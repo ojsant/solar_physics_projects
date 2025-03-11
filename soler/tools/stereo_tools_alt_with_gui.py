1917dc740aadf606b6a9dad67b1954e808e4a672
ojsant <ojsant@utu.fi>
2025-03-11 15:22:45 +0200

    .

--- soler/tools/stereo_tools_alt_with_gui.py
+++ soler/tools/stereo_tools_alt_with_gui.py
@@ -6,6 +6,8 @@ import pandas as pd
 import warnings
 import math
 import cdflib
+import sys
+import sunpy
 
 from matplotlib import pyplot as plt
 plt.rcParams['axes.linewidth'] = 1.5
@@ -40,11 +42,13 @@ import tools.cdaweb as cdaweb
 
 # omit Pandas' PerformanceWarning
 warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
+warnings.filterwarnings(action='ignore', message='No units provided for variable', category=sunpy.util.SunpyUserWarning, module='sunpy.io._cdf')
+warnings.filterwarnings(action='ignore', message='astropy did not recognize units of', category=sunpy.util.SunpyUserWarning, module='sunpy.io._cdf')
 
 
 def load_swaves(dataset, startdate, enddate, path=None):
     """
-    Load PSP Fields data from CDAWeb. Combines time if timespan is multiple days.
+    Load STA WAVES data from CDAWeb.
 
     Parameters
     ----------
@@ -66,7 +70,7 @@ def load_swaves(dataset, startdate, enddate, path=None):
     files = cdaweb.cdaweb_download_fido(dataset=dataset, startdate=startdate, enddate=enddate, path=path)
 
     if len(files) == 0:
-        print(f"No radio data found for {startdate} - {enddate}")
+        print(f"No WAVES radio data found between {startdate} and {enddate}")
         return pd.DataFrame()
     else:
         freq_mhz = cdflib.CDF(files[0]).varget("FREQUENCY") / 1e6
@@ -113,6 +117,7 @@ def load_data(opt):
     meta_se = None
     meta_sp = None
     meta_het = None
+    df_magplasma = None
 
     startdate = opt["startdate"]
     enddate = opt["enddate"]
@@ -126,12 +131,11 @@ def load_data(opt):
     plot_N = opt["N"].value
     plot_T = opt["T"].value
     plot_het = opt["HET"].value
+    plot_polarity = opt["Polarity"].value
 
     resample = opt["resample"]
     resample_mag = opt["resample_mag"]
-    n_sept_e = opt["n_sept_e"]
-    n_sept_p = opt["n_sept_p"]
-    n_het_p = opt["n_het_p"]
+    resample_pol = opt["resample_pol"]
 
     df_sept_electrons_orig, meta_se = stereo_load(instrument='SEPT', startdate=startdate, enddate=enddate, 
                         sept_species='e', sept_viewing=sept_viewing,
@@ -139,6 +143,7 @@ def load_data(opt):
     df_sept_protons_orig, meta_sp = stereo_load(instrument='SEPT', startdate=startdate, enddate=enddate, 
                             sept_species='p', sept_viewing=sept_viewing,
                             path=file_path, pos_timestamp=pos_timestamp, spacecraft=sc)
+    
     if plot_het:
         df_het_orig, meta_het = stereo_load(instrument='HET', startdate=startdate, enddate=enddate,
                         path=file_path, pos_timestamp=pos_timestamp, spacecraft=sc)
@@ -148,13 +153,13 @@ def load_data(opt):
                                         path=file_path)
 
     if plot_Vsw or plot_N or plot_T:
-        magplasma, meta_magplas = stereo_load(instrument='MAGPLASMA', startdate=startdate, enddate=enddate, 
+        df_magplasma, meta_magplas = stereo_load(instrument='MAGPLASMA', startdate=startdate, enddate=enddate, 
                             path=file_path, pos_timestamp=pos_timestamp, spacecraft=sc)
-        
+      
+
     if plot_radio:
         df_waves_hfr = load_swaves("STA_L3_WAV_HFR", startdate=startdate, enddate=enddate, path=file_path)
         df_waves_lfr = load_swaves("STA_L3_WAV_LFR", startdate=startdate, enddate=enddate, path=file_path)
-
 
     if resample is not None:
         df_sept_electrons = resample_df(df_sept_electrons_orig, resample)  
@@ -162,9 +167,11 @@ def load_data(opt):
         if plot_het:
             df_het  = resample_df(df_het_orig, resample)  
         if plot_Vsw or plot_N or plot_T:
-            df_magplas = resample_df(magplasma, resample_mag) 
+            df_magplas = resample_df(df_magplasma, resample_mag) 
         if plot_mag:
             df_mag = resample_df(df_mag_orig, resample_mag)
+        if plot_polarity:
+            df_magplas_pol = resample_df(df_magplasma, resample_pol)
     
     else:
         df_sept_electrons = df_sept_electrons_orig
@@ -172,24 +179,11 @@ def load_data(opt):
         if plot_het:
             df_het  = df_het_orig
         if plot_Vsw or plot_N or plot_T:
-            df_magplas = magplasma
+            df_magplas = df_magplasma
         if plot_mag:
             df_mag = df_mag_orig
-
-    #Channels list
-    channels_n_sept_e = list(np.arange(0,14+1,n_sept_e)) # why not just range()??
-    channels_n_het_e = list(np.arange(0,2+1,1))
-    channels_n_sept_p = list(np.arange(0,29+1,n_sept_p))
-    channels_n_het_p = list(np.arange(0,10+1,n_het_p))
-
-    channels_list = [channels_n_sept_e, channels_n_het_e, channels_n_sept_p, channels_n_het_p]
-
-    #Chosen channels
-    print('Channels:')
-    print('sept_e:',channels_list[0],',', len(channels_list[0]))
-    print('het_e:',channels_list[1],',', len(channels_list[1]))
-    print('sept_p:',channels_list[2],',', len(channels_list[2]))
-    print('het_p:',channels_list[3],',', len(channels_list[3]))
+        if plot_polarity:
+            df_magplas_pol = df_magplasma
 
     data = {}
     data["meta_se"] = meta_se
@@ -203,19 +197,16 @@ def load_data(opt):
     data["df_het"] = df_het
     data["df_mag"] = df_mag
     data["df_magplas"] = df_magplas
-    data["df_magplas_pol"] = magplasma  # !!!!!! un-resampled for polarity plotting 
-
-    opt["channels_list"] = channels_list
+    data["df_magplas_pol"] = df_magplas_pol
  
     return data
 
 def make_plot(data, opt):
-    FONT_YLABEL = 20
-    FONT_LEGEND = 10
-    
+    font_ylabel = 20
+    font_legend = 10
+
     meta_se = data["meta_se"] 
     meta_sp = data["meta_sp"] 
-    
     meta_het = data["meta_het"] 
     
     df_waves_hfr = data["df_waves_hfr"] 
@@ -244,8 +235,29 @@ def make_plot(data, opt):
     plot_N = opt["N"].value
     plot_T = opt["T"].value
     plot_het = opt["HET"].value
-    
-    channels_list = opt["channels_list"]
+    n_sept_e = opt["n_sept_e"]
+    n_sept_p = opt["n_sept_p"]
+    n_het_p = opt["n_het_p"]
+
+    legends_inside = opt["legends_inside"]
+
+    t_start = opt["plot_start"]
+    t_end = opt["plot_end"]
+
+    #Channels list
+    channels_n_sept_e = list(np.arange(0,14+1,n_sept_e)) # why not just range()??
+    channels_n_het_e = list(np.arange(0,2+1,1))
+    channels_n_sept_p = list(np.arange(0,29+1,n_sept_p))
+    channels_n_het_p = list(np.arange(0,10+1,n_het_p))
+
+    channels_list = [channels_n_sept_e, channels_n_het_e, channels_n_sept_p, channels_n_het_p]
+
+    #Chosen channels
+    print('Channels:')
+    print('sept_e:',channels_list[0],',', len(channels_list[0]))
+    print('het_e:',channels_list[1],',', len(channels_list[1]))
+    print('sept_p:',channels_list[2],',', len(channels_list[2]))
+    print('het_p:',channels_list[3],',', len(channels_list[3]))
 
     panels = 1*plot_radio + 1*plot_electrons + 1*plot_protons  + 2*plot_mag_angles + 1*plot_mag + 1* plot_Vsw + 1* plot_N + 1* plot_T # + 1*plot_pad
 
@@ -282,12 +294,12 @@ def make_plot(data, opt):
         axs[i].pcolormesh(TimeHFR2D, FreqHFR2D, df_waves_hfr.iloc[:-1,:-1], shading='flat', cmap='jet', norm=log_norm) # TODO: check if on top
 
         axs[i].set_yscale('log')
-        axs[i].set_ylabel("Frequency (MHz)", fontsize=FONT_YLABEL)
+        axs[i].set_ylabel("Frequency (MHz)", fontsize=font_ylabel)
         
         # Add inset axes for colorbar
         axins = inset_axes(axs[i], width="100%", height="100%", loc="center", bbox_to_anchor=(1.05,0,0.03,1), bbox_transform=axs[i].transAxes, borderpad=0.2)
         cbar = fig.colorbar(mesh, cax=axins, orientation="vertical")
-        cbar.set_label("Intensity (sfu)", rotation=90, labelpad=10, fontsize=FONT_YLABEL)
+        cbar.set_label("Intensity (sfu)", rotation=90, labelpad=10, fontsize=font_ylabel)
         i += 1
 
     if plot_electrons:
@@ -304,13 +316,13 @@ def make_plot(data, opt):
                             label='HET '+meta_het['channels_dict_df_e'].ch_strings[channel],
                         ds="steps-mid")
         
-        axs[i].set_ylabel("Flux\n"+r"[(cm$^2$ sr s MeV)$^{-1}]$", fontsize=FONT_YLABEL)
-        if opt["legends_inside"]:
+        axs[i].set_ylabel("Flux\n"+r"[(cm$^2$ sr s MeV)$^{-1}]$", fontsize=font_ylabel)
+        if legends_inside:
             axs[i].legend(loc='upper right', borderaxespad=0., 
-                    title=f'Electrons (SEPT: {sept_viewing}, HET: sun)', fontsize=FONT_LEGEND)
+                    title=f'Electrons (SEPT: {sept_viewing}, HET: sun)', fontsize=font_legend)
         else:
             axs[i].legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., 
-                    title=f'Electrons (SEPT: {sept_viewing}, HET: sun)', fontsize=FONT_LEGEND)
+                    title=f'Electrons (SEPT: {sept_viewing}, HET: sun)', fontsize=font_legend)
         axs[i].set_yscale('log')
         i +=1    
 
@@ -332,20 +344,15 @@ def make_plot(data, opt):
                 axs[i].plot(df_het.index, df_het[f'Proton_Flux_{channel}'], 
                         label='HET '+meta_het['channels_dict_df_p'].ch_strings[channel], ds="steps-mid")
         
-        axs[i].set_ylabel("Flux\n"+r"[(cm$^2$ sr s MeV)$^{-1}]$", fontsize=FONT_YLABEL)
-        if opt["legends_inside"]:
+        axs[i].set_ylabel("Flux\n"+r"[(cm$^2$ sr s MeV)$^{-1}]$", fontsize=font_ylabel)
+        if legends_inside:
             axs[i].legend(loc='upper right', borderaxespad=0., 
-                    title=f'Ions (SEPT: {sept_viewing}, HET: sun)', fontsize=FONT_LEGEND)
+                    title=f'Ions (SEPT: {sept_viewing}, HET: sun)', fontsize=font_legend)
         else:
             axs[i].legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., 
-                    title=f'Ions (SEPT: {sept_viewing}, HET: sun)', fontsize=FONT_LEGEND)
+                    title=f'Ions (SEPT: {sept_viewing}, HET: sun)', fontsize=font_legend)
         axs[i].set_yscale('log')
         i +=1    
-        
-        
-        
-        
-        
         
     # plot magnetic field
     if plot_mag:
@@ -355,12 +362,12 @@ def make_plot(data, opt):
         ax.plot(df_mag.index.values, df_mag.BFIELD_1.values, label='Bt', color='limegreen')
         ax.plot(df_mag.index.values, df_mag.BFIELD_2.values, label='Bn', color='deeppink')
         ax.axhline(y=0, color='gray', linewidth=0.8, linestyle='--')
-        if opt["legends_inside"]:
+        if legends_inside:
             ax.legend(loc='upper right')
         else:
             ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
             
-        ax.set_ylabel('B [nT]', fontsize=FONT_YLABEL)
+        ax.set_ylabel('B [nT]', fontsize=font_ylabel)
         ax.tick_params(axis="x", direction="in", which='both')#, pad=-15)
         i += 1
         
@@ -371,7 +378,7 @@ def make_plot(data, opt):
         r = np.interp([t.timestamp() for t in df_magplas_pol.index],[t.timestamp() for t in pd.to_datetime(pos.obstime.value)],pos.radius.value)
         lat = np.interp([t.timestamp() for t in df_magplas_pol.index],[t.timestamp() for t in pd.to_datetime(pos.obstime.value)],pos.lat.value)
         pol, phi_relative = polarity_rtn(df_magplas_pol.BFIELDRTN_0.values, df_magplas_pol.BFIELDRTN_1.values, df_magplas_pol.BFIELDRTN_2.values,r,lat,V=400)
-    # create an inset axe in the current axe:
+        # create an inset axe in the current axe:
         pol_ax = inset_axes(ax, height="5%", width="100%", loc=9, bbox_to_anchor=(0.,0,1,1.1), bbox_transform=ax.transAxes) # center, you can check the different codes in plt.legend?
         pol_ax.get_xaxis().set_visible(False)
         pol_ax.get_yaxis().set_visible(False)
@@ -383,7 +390,7 @@ def make_plot(data, opt):
         mapper = cm.ScalarMappable(norm=norm, cmap=cm.bwr)
         pol_ax.bar(df_magplas_pol.index.values[(phi_relative>=0) & (phi_relative<180)],pol_arr[(phi_relative>=0) & (phi_relative<180)],color=mapper.to_rgba(phi_relative[(phi_relative>=0) & (phi_relative<180)]),width=timestamp)
         pol_ax.bar(df_magplas_pol.index.values[(phi_relative>=180) & (phi_relative<360)],pol_arr[(phi_relative>=180) & (phi_relative<360)],color=mapper.to_rgba(np.abs(360-phi_relative[(phi_relative>=180) & (phi_relative<360)])),width=timestamp)
-        pol_ax.set_xlim(startdate, enddate)
+        pol_ax.set_xlim(t_start, t_end)
         
     if plot_mag_angles:
         ax = axs[i]
@@ -393,7 +400,7 @@ def make_plot(data, opt):
         ax.plot(df_mag.index, alpha, '.k', label='alpha', ms=1)
         ax.axhline(y=0, color='gray', linewidth=0.8, linestyle='--')
         ax.set_ylim(-90, 90)
-        ax.set_ylabel(r"$\Theta_\mathrm{B}$ [°]", fontsize=FONT_YLABEL)
+        ax.set_ylabel(r"$\Theta_\mathrm{B}$ [°]", fontsize=font_ylabel)
         # ax.set_xlim(X1, X2)
         ax.tick_params(axis="x",direction="in", pad=-15)
 
@@ -402,7 +409,7 @@ def make_plot(data, opt):
         ax.plot(df_mag.index, phi, '.k', label='phi', ms=1)
         ax.axhline(y=0, color='gray', linewidth=0.8, linestyle='--')
         ax.set_ylim(-180, 180)
-        ax.set_ylabel(r"$\Phi_\mathrm{B}$ [°]", fontsize=FONT_YLABEL)
+        ax.set_ylabel(r"$\Phi_\mathrm{B}$ [°]", fontsize=font_ylabel)
         # ax.set_xlim(X1, X2)
         ax.tick_params(axis="x",direction="in", which='both', pad=-15)
         i += 1
@@ -410,7 +417,7 @@ def make_plot(data, opt):
     ### Temperature
     if plot_T:
         axs[i].plot(df_magplas.index, df_magplas['Tp'], '-k', label="Temperature")
-        axs[i].set_ylabel(r"T$_\mathrm{p}$ [K]", fontsize=FONT_YLABEL)
+        axs[i].set_ylabel(r"T$_\mathrm{p}$ [K]", fontsize=font_ylabel)
         axs[i].set_yscale('log')
         i += 1
 
@@ -418,24 +425,22 @@ def make_plot(data, opt):
     if plot_N:
         axs[i].plot(df_magplas.index, df_magplas.Np,
                     '-k', label="Ion density")
-        axs[i].set_ylabel(r"N$_\mathrm{p}$ [cm$^{-3}$]", fontsize=FONT_YLABEL)
+        axs[i].set_ylabel(r"N$_\mathrm{p}$ [cm$^{-3}$]", fontsize=font_ylabel)
         i += 1
 
     ### Sws
     if plot_Vsw:
         axs[i].plot(df_magplas.index, df_magplas.Vp,
                     '-k', label="Bulk speed")
-        axs[i].set_ylabel(r"V$_\mathrm{sw}$ [kms$^{-1}$]", fontsize=FONT_YLABEL)
+        axs[i].set_ylabel(r"V$_\mathrm{sw}$ [kms$^{-1}$]", fontsize=font_ylabel)
         #i += 1
         
-            
-    #axs[-1].set_xlabel(f"Date in {year}/  Time (UTC)", fontsize=15)
-    #axs[-1].set_xlim(startdate, enddate)
     axs[0].set_title(f'STEREO {sc}', ha='center')
-    axs[-1].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M\n%b %#d'))
+
+    axs[-1].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M\n%b %d'))
     axs[-1].xaxis.set_tick_params(rotation=0)
-    axs[-1].set_xlabel(f"Time (UTC) / Date in {startdate.year}", fontsize=15)
-    axs[-1].set_xlim(startdate, enddate)
+    axs[-1].set_xlabel(f"Time (UTC) / Date in {t_start.year}", fontsize=15)
+    axs[-1].set_xlim(t_start, t_end)
 
     #plt.tight_layout()
     #fig.set_size_inches(12,15)
@@ -447,10 +452,32 @@ def make_plot(data, opt):
 
 
 def selection():
-    options = ["Radio", "Electrons", "Protons", "PAD", "Mag angles", "Mag", "V_sw", "N", "T", "Polarity", "HET"]
+    options = ["Radio", "Electrons", "Protons", "PAD", "MAG angles", "MAG", "V_sw", "N", "T", "Polarity", "HET"]
     checkboxes = dict(zip(options, [w.Checkbox(value=False, description=option, indent=False) for option in options]))
     for option in options:
         display(checkboxes[option])
-
+    
     return checkboxes
-        
+
+def plot_range_selection(opt):
+    
+    data_start = opt["startdate"]
+    data_end = opt["enddate"]
+    
+    plot_start = w.NaiveDatetimePicker(description="Plot range start", value=data_start, min=data_start, max=data_end, disabled=False)
+    plot_end = w.NaiveDatetimePicker(description="Plot range end", value=data_end, min=data_start, max=data_end, disabled=False)
+
+    opt["plot_start"] = plot_start.value
+    opt["plot_end"] = plot_end.value
+    
+    display(plot_start)
+    display(plot_end)
+
+    
+    
+# startdate = opt["startdate"]
+# enddate = opt["enddate"]
+# sept_viewing = opt["sept_viewing"]
+# pos_timestamp = opt["pos_timestamp"]
+# file_path = opt["file_path"]
+# sc = opt["sc"]
