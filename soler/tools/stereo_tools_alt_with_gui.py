f12f02145aeaa48ebcf3f1d695f7b9256397e5fe
ojsant <ojsant@utu.fi>
2025-03-12 15:25:21 +0200

    Implemented full widget GUI for STEREO

--- soler/tools/stereo_tools_alt_with_gui.py
+++ soler/tools/stereo_tools_alt_with_gui.py
@@ -46,9 +46,118 @@ warnings.filterwarnings(action='ignore', message='No units provided for variable
 warnings.filterwarnings(action='ignore', message='astropy did not recognize units of', category=sunpy.util.SunpyUserWarning, module='sunpy.io._cdf')
 
 
+class Options:
+    
+    def __init__(self):
+        style = {'description_width' : '50%'}
+        layout = w.Layout(width="20%")
+        
+        self.radio = w.Checkbox(value=True, description="SWAVES LFR/HFR", indent=False, style=style, layout=layout)
+        self.electrons = w.Checkbox(value=True, description="Electrons", indent=False, style=style, layout=layout)
+        self.het = w.Checkbox(value=True, description="HET", indent=False, style=style, layout=layout)
+        self.protons = w.Checkbox(value=True, description="Protons", indent=False, style=style, layout=layout)
+        self.pad = w.Checkbox(value=False, description="PAD (not implemented)", disabled=True, indent=False, style=style, layout=layout)    # TODO: remove disabled keyword after implementation
+        self.mag = w.Checkbox(value=False, description="MAG", indent=False, style=style, layout=layout)
+        self.mag_angles = w.Checkbox(value=True, description="MAG angles", indent=False, style=style, layout=layout)
+        self.Vsw = w.Checkbox(value=True, description="V_sw", indent=False, style=style, layout=layout)
+        self.N = w.Checkbox(value=True, description="N", indent=False, style=style, layout=layout)
+        self.T = w.Checkbox(value=True, description="T", indent=False, style=style, layout=layout)
+        self.polarity = w.Checkbox(value=True, description="Polarity", indent=False, style=style, layout=layout)
+        self.startdate = w.NaiveDatetimePicker(value=None, disabled=False, min=dt.datetime(2006, 10, 26), max=dt.datetime.now() - dt.timedelta(hours=1), description="Start date (data):", style=style, layout=layout)
+        self.enddate = w.NaiveDatetimePicker(value=None, disabled=False, min=dt.datetime(2006, 10, 26) + dt.timedelta(hours=1), max=dt.datetime.now(), description="End date (data):", style=style, layout=layout)
+        self.sept_viewing = w.Dropdown(options=['sun', 'asun', 'north', 'south'], value='sun', description='SEPT viewing:', style=style, layout=layout)
+        self.sc = w.Dropdown(options=['A', 'B'], value='A', description='Spacecraft (A or B):', style=style, layout=layout)
+        self.n_sept_e = w.SelectMultiple(options=range(0,14+1), description='SEPT electron energies:', rows=10, style=style, layout=layout)
+        self.n_sept_p = w.SelectMultiple(options=range(0,29+1), description='SEPT proton energies:', rows=10, style=style, layout=layout)
+        self.n_het_p =  w.SelectMultiple(options=range(0,10+1), description='HET proton energies:', rows=10, style=style, layout=layout)
+        self.legends_inside = w.Checkbox(value=False, description='Legends inside', indent=False, style=style, layout=layout)
+        self.resample = w.BoundedIntText(value=15, min=0, max=30, step=1, description='Resampling (min):', disabled=False, style=style, layout=layout)
+        self.resample_mag = w.BoundedIntText(value=5, min=0, max=30, step=1, description='MAG resampling (min):', disabled=False, style=style, layout=layout)
+        self.resample_pol = w.BoundedIntText(value=1, min=0, max=30, step=1, description='Polarity resampling (min):', disabled=False, style=style, layout=layout)
+        self.pos_timestamp = w.Dropdown(options=['center', 'start', 'original'], description='Timestamp position', style=style, layout=layout)
+        self.path = None
+        self.plot_range = None
+        
+    def disp(self):
+        for attr in vars(self):
+            if attr not in ["path", "plot_range"]:
+                display(vars(self)[attr])
+
+    # for testing purposes
+    def set_test_values(self):
+        self.radio.value = True
+        self.electrons.value  = True
+        self.Vsw.value = True
+        self.N.value = True
+        self.startdate.value = dt.datetime(2022,3,14,00)
+        self.enddate.value = dt.datetime(2022,3,16,00)
+        self.n_het_p.value = (4, 7, 8, 9)
+        self.n_sept_p.value = (1, 2, 3, 7, 8, 9, 10, 11, 19, 20, 21)
+        self.n_sept_e.value = (3, 4, 5, 6, 7, 8, 12, 13, 14)
+        
+
+def plot_range_interval(startdate, enddate):
+    timestamps = []
+    date_iter = startdate
+    while date_iter <= enddate:
+        timestamps.append(date_iter)
+        date_iter = date_iter + dt.timedelta(hours=1)
+    return timestamps
+
+
+def date_selector(startdate=None, enddate=None):
+    """
+    An HTML work-around to display datetimes without clipping on SelectionRangeSlider readouts.
+
+    Author: Marcus Reaiche (https://github.com/jupyter-widgets/ipywidgets/issues/2855#issuecomment-966747483)
+    """
+    
+    # Define date range
+    if startdate is None and enddate is None:
+        dates = plot_range_interval(startdate=options.startdate.value, enddate=options.enddate.value)
+
+    else:
+        dates = plot_range_interval(startdate=startdate, enddate=enddate)
+
+    # First and last dates are selected by default
+    initial_selection = (0, len(dates) - 1)
+
+    # Define the date range slider: set readout to False
+    date_range_selector = w.SelectionRangeSlider(
+        options=dates,
+        description="Plot range",
+        index=initial_selection,
+        continous_update=False,
+        readout=False
+    )
+
+    # Define the display to substitute the readout
+    date_range_display = w.HTML(
+        value=(
+            f"<b>{dates[initial_selection[0]]}" + 
+            f" - {dates[initial_selection[1]]}</b>"))
+
+    # Define the date range using the widgets.HBox
+    date_range = w.HBox(
+        (date_range_selector, date_range_display))
+
+    # Callback function that updates the display
+    def callback(dts):
+        date_range_display.value = f"<b>{dts[0]} - {dts[1]}</b>"
+
+    w.interactive_output(
+        callback, 
+        {"dts": date_range_selector})
+    
+    options.plot_range = date_range
+    return date_range
+
+
+
+
 def load_swaves(dataset, startdate, enddate, path=None):
     """
-    Load STA WAVES data from CDAWeb.
+    Load STEREO/WAVES data from CDAWeb.
 
     Parameters
     ----------
@@ -70,7 +179,7 @@ def load_swaves(dataset, startdate, enddate, path=None):
     files = cdaweb.cdaweb_download_fido(dataset=dataset, startdate=startdate, enddate=enddate, path=path)
 
     if len(files) == 0:
-        print(f"No WAVES radio data found between {startdate} and {enddate}")
+        print(f"No SWAVES radio data found between {startdate} and {enddate}")
         return pd.DataFrame()
     else:
         freq_mhz = cdflib.CDF(files[0]).varget("FREQUENCY") / 1e6
@@ -101,65 +210,77 @@ def load_swaves(dataset, startdate, enddate, path=None):
     return psd_sfu
 
 
+def load_data():
+    global df_sept_electrons_orig
+    global df_sept_protons_orig
+    global df_het_orig
+    global df_waves_hfr
+    global df_waves_lfr
+    global df_sept_electrons
+    global df_sept_protons
+    global df_het
+    global df_mag
+    global df_magplas
+    global meta_se
+    global meta_sp
+    global meta_het
+    global df_magplas_pol
 
-def load_data(opt):
+    global startdate
+    global enddate
+    global sept_viewing
+    global pos_timestamp
+    global path
+    global sc
+    global plot_radio
+    global plot_mag
+    global plot_Vsw
+    global plot_N
+    global plot_T
+    global plot_het
+    global plot_polarity
 
-    df_sept_electrons_orig = None
-    df_sept_protons_orig = None
-    df_het_orig = None
-    df_waves_hfr = None
-    df_waves_lfr = None
-    df_sept_electrons = None
-    df_sept_protons = None
-    df_het = None
-    df_mag = None
-    df_magplas = None
-    meta_se = None
-    meta_sp = None
-    meta_het = None
-    df_magplasma = None
+    startdate = options.startdate.value
+    enddate = options.enddate.value
+    sept_viewing = options.sept_viewing.value
+    pos_timestamp = options.pos_timestamp.value
+    path = options.path
+    sc = options.sc.value
+    plot_radio = options.radio.value
+    plot_mag = options.mag.value
+    plot_Vsw = options.Vsw.value
+    plot_N = options.N.value
+    plot_T = options.T.value
+    plot_het = options.het.value
+    plot_polarity = options.polarity.value
 
-    startdate = opt["startdate"]
-    enddate = opt["enddate"]
-    sept_viewing = opt["sept_viewing"]
-    pos_timestamp = opt["pos_timestamp"]
-    file_path = opt["file_path"]
-    sc = opt["sc"]
-    plot_radio = opt["Radio"].value
-    plot_mag = opt["Mag"].value
-    plot_Vsw = opt["V_sw"].value
-    plot_N = opt["N"].value
-    plot_T = opt["T"].value
-    plot_het = opt["HET"].value
-    plot_polarity = opt["Polarity"].value
-
-    resample = opt["resample"]
-    resample_mag = opt["resample_mag"]
-    resample_pol = opt["resample_pol"]
+    resample = str(options.resample.value) + "min"         # convert to form that Pandas accepts
+    resample_mag = str(options.resample_mag.value) + "min"
+    resample_pol = str(options.resample_pol.value) + "min"
 
     df_sept_electrons_orig, meta_se = stereo_load(instrument='SEPT', startdate=startdate, enddate=enddate, 
                         sept_species='e', sept_viewing=sept_viewing,
-                        path=file_path, pos_timestamp=pos_timestamp, spacecraft=sc)
+                        path=path, pos_timestamp=pos_timestamp, spacecraft=sc)
     df_sept_protons_orig, meta_sp = stereo_load(instrument='SEPT', startdate=startdate, enddate=enddate, 
                             sept_species='p', sept_viewing=sept_viewing,
-                            path=file_path, pos_timestamp=pos_timestamp, spacecraft=sc)
+                            path=path, pos_timestamp=pos_timestamp, spacecraft=sc)
     
     if plot_het:
         df_het_orig, meta_het = stereo_load(instrument='HET', startdate=startdate, enddate=enddate,
-                        path=file_path, pos_timestamp=pos_timestamp, spacecraft=sc)
+                        path=path, pos_timestamp=pos_timestamp, spacecraft=sc)
 
     if plot_mag:
         df_mag_orig, meta_mag = stereo_load(spacecraft=sc, instrument='MAG', startdate=startdate, enddate=enddate, mag_coord='RTN', 
-                                        path=file_path)
+                                        path=path)
 
     if plot_Vsw or plot_N or plot_T:
         df_magplasma, meta_magplas = stereo_load(instrument='MAGPLASMA', startdate=startdate, enddate=enddate, 
-                            path=file_path, pos_timestamp=pos_timestamp, spacecraft=sc)
+                            path=path, pos_timestamp=pos_timestamp, spacecraft=sc)
       
 
     if plot_radio:
-        df_waves_hfr = load_swaves("STA_L3_WAV_HFR", startdate=startdate, enddate=enddate, path=file_path)
-        df_waves_lfr = load_swaves("STA_L3_WAV_LFR", startdate=startdate, enddate=enddate, path=file_path)
+        df_waves_hfr = load_swaves("STA_L3_WAV_HFR", startdate=startdate, enddate=enddate, path=path)
+        df_waves_lfr = load_swaves("STA_L3_WAV_LFR", startdate=startdate, enddate=enddate, path=path)
 
     if resample is not None:
         df_sept_electrons = resample_df(df_sept_electrons_orig, resample)  
@@ -185,79 +306,42 @@ def load_data(opt):
         if plot_polarity:
             df_magplas_pol = df_magplasma
 
-    data = {}
-    data["meta_se"] = meta_se
-    data["meta_sp"] = meta_sp
-    data["meta_het"] = meta_het
-    
-    data["df_waves_hfr"] = df_waves_hfr
-    data["df_waves_lfr"] = df_waves_lfr
-    data["df_sept_electrons"] = df_sept_electrons
-    data["df_sept_protons"] = df_sept_protons
-    data["df_het"] = df_het
-    data["df_mag"] = df_mag
-    data["df_magplas"] = df_magplas
-    data["df_magplas_pol"] = df_magplas_pol
- 
-    return data
 
-def make_plot(data, opt):
+
+def make_plot():
     font_ylabel = 20
     font_legend = 10
-
-    meta_se = data["meta_se"] 
-    meta_sp = data["meta_sp"] 
-    meta_het = data["meta_het"] 
     
-    df_waves_hfr = data["df_waves_hfr"] 
-    df_waves_lfr = data["df_waves_lfr"] 
-    df_sept_electrons = data["df_sept_electrons"] 
-    df_sept_protons = data["df_sept_protons"] 
-    df_het = data["df_het"] 
-    df_mag = data["df_mag"] 
-    df_magplas = data["df_magplas"]
-    df_magplas_pol = data["df_magplas_pol"]
-
-    startdate = opt["startdate"]
-    enddate = opt["enddate"]
-    sept_viewing = opt["sept_viewing"]
+    plot_electrons = options.electrons.value
+    plot_protons = options.protons.value
+    plot_mag_angles = options.mag_angles.value
     
-    sc = opt["sc"]
+    n_sept_e = options.n_sept_e.value
+    n_sept_p = options.n_sept_p.value
+    n_het_p = options.n_het_p.value
+    n_het_e = range(0,2+1,1)
 
-    plot_electrons = opt["Electrons"].value
-    plot_protons = opt["Protons"].value
-    plot_pad = opt["PAD"].value
-    plot_mag_angles = opt["Mag angles"].value
-    plot_polarity = opt["Polarity"].value
-    plot_radio = opt["Radio"].value
-    plot_mag = opt["Mag"].value
-    plot_Vsw = opt["V_sw"].value
-    plot_N = opt["N"].value
-    plot_T = opt["T"].value
-    plot_het = opt["HET"].value
-    n_sept_e = opt["n_sept_e"]
-    n_sept_p = opt["n_sept_p"]
-    n_het_p = opt["n_het_p"]
+    legends_inside = options.legends_inside.value
 
-    legends_inside = opt["legends_inside"]
+    t_start = options.plot_range.children[0].value[0]
+    t_end = options.plot_range.children[0].value[1]
 
-    t_start = opt["plot_start"]
-    t_end = opt["plot_end"]
+    print(f"Chosen plot range:\n{t_start} - {t_end}")
 
-    #Channels list
-    channels_n_sept_e = list(np.arange(0,14+1,n_sept_e)) # why not just range()??
-    channels_n_het_e = list(np.arange(0,2+1,1))
-    channels_n_sept_p = list(np.arange(0,29+1,n_sept_p))
-    channels_n_het_p = list(np.arange(0,10+1,n_het_p))
+    # #Channels list
+    # channels_n_sept_e = range(0,14+1,n_sept_e)  # changed from np.arange()
+    # channels_n_het_e = range(0,2+1,1)
+    # channels_n_sept_p = range(0,29+1,n_sept_p)
+    # channels_n_het_p = range(0,10+1,n_het_p)
 
-    channels_list = [channels_n_sept_e, channels_n_het_e, channels_n_sept_p, channels_n_het_p]
+    # channels_list = [channels_n_sept_e, channels_n_het_e, channels_n_sept_p, channels_n_het_p]
 
     #Chosen channels
-    print('Channels:')
-    print('sept_e:',channels_list[0],',', len(channels_list[0]))
-    print('het_e:',channels_list[1],',', len(channels_list[1]))
-    print('sept_p:',channels_list[2],',', len(channels_list[2]))
-    print('het_p:',channels_list[3],',', len(channels_list[3]))
+    print('Chosen channels:')
+    print(f'SEPT electrons: {n_sept_e}, {len(n_sept_e)}')
+    print(f'HET electrons: {n_het_e}, {len(n_het_e)}')
+    print(f'SEPT protons: {n_sept_p}, {len(n_sept_p)}')
+    print(f'HET protons: {n_het_p}, {len(n_het_p)}')
 
     panels = 1*plot_radio + 1*plot_electrons + 1*plot_protons  + 2*plot_mag_angles + 1*plot_mag + 1* plot_Vsw + 1* plot_N + 1* plot_T # + 1*plot_pad
 
@@ -304,14 +388,14 @@ def make_plot(data, opt):
 
     if plot_electrons:
         # plot sept electron channels
-        axs[i].set_prop_cycle('color', plt.cm.Reds_r(np.linspace(0,1,len(channels_list[0])+color_offset)))
-        for channel in channels_list[0]:
+        axs[i].set_prop_cycle('color', plt.cm.Reds_r(np.linspace(0,1,len(n_sept_e)+color_offset)))
+        for channel in n_sept_e:
             axs[i].plot(df_sept_electrons.index, df_sept_electrons[f'ch_{channel+2}'],
                         ds="steps-mid", label='SEPT '+meta_se.ch_strings[channel+2])
         if plot_het:
             # plot het electron channels
             axs[i].set_prop_cycle('color', plt.cm.PuRd_r(np.linspace(0,1,4+color_offset)))
-            for channel in channels_list[1]:
+            for channel in n_het_e:
                 axs[i].plot(df_het[f'Electron_Flux_{channel}'], 
                             label='HET '+meta_het['channels_dict_df_e'].ch_strings[channel],
                         ds="steps-mid")
@@ -330,17 +414,17 @@ def make_plot(data, opt):
     color_offset = 2    
     if plot_protons:
         # plot sept proton channels
-        num_channels = len(channels_list[2])# + len(channels_list[3])
+        num_channels = len(n_sept_p)# + len(n_het_p)
         axs[i].set_prop_cycle('color', plt.cm.plasma(np.linspace(0,1,num_channels+color_offset)))
-        for channel in channels_list[2]:
+        for channel in n_sept_p:
             axs[i].plot(df_sept_protons.index, df_sept_protons[f'ch_{channel+2}'], 
                     label='SEPT '+meta_sp.ch_strings[channel+2], ds="steps-mid")
         
         color_offset = 0 
         if plot_het:
             # plot het proton channels
-            axs[i].set_prop_cycle('color', plt.cm.YlOrRd(np.linspace(0.2,1,len(channels_list[3])+color_offset)))
-            for channel in channels_list[3]:
+            axs[i].set_prop_cycle('color', plt.cm.YlOrRd(np.linspace(0.2,1,len(n_het_p)+color_offset)))
+            for channel in n_het_p:
                 axs[i].plot(df_het.index, df_het[f'Proton_Flux_{channel}'], 
                         label='HET '+meta_het['channels_dict_df_p'].ch_strings[channel], ds="steps-mid")
         
@@ -450,34 +534,4 @@ def make_plot(data, opt):
 
     return fig, axs
 
-
-def selection():
-    options = ["Radio", "Electrons", "Protons", "PAD", "MAG angles", "MAG", "V_sw", "N", "T", "Polarity", "HET"]
-    checkboxes = dict(zip(options, [w.Checkbox(value=False, description=option, indent=False) for option in options]))
-    for option in options:
-        display(checkboxes[option])
-    
-    return checkboxes
-
-def plot_range_selection(opt):
-    
-    data_start = opt["startdate"]
-    data_end = opt["enddate"]
-    
-    plot_start = w.NaiveDatetimePicker(description="Plot range start", value=data_start, min=data_start, max=data_end, disabled=False)
-    plot_end = w.NaiveDatetimePicker(description="Plot range end", value=data_end, min=data_start, max=data_end, disabled=False)
-
-    opt["plot_start"] = plot_start.value
-    opt["plot_end"] = plot_end.value
-    
-    display(plot_start)
-    display(plot_end)
-
-    
-    
-# startdate = opt["startdate"]
-# enddate = opt["enddate"]
-# sept_viewing = opt["sept_viewing"]
-# pos_timestamp = opt["pos_timestamp"]
-# file_path = opt["file_path"]
-# sc = opt["sc"]
+options = Options()
